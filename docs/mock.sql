9673f79c289db4d84d200cafd0377971053c8e93
Bill <Bill@TheWallisFamily.co.uk>
2025-12-02 13:57:33 +0000

    feat: add more extractors (#5)

--- docs/mock.sql
+++ docs/mock.sql
@@ -1,17 +1,15 @@
-
-WITH mock_data(id, name, date_of_birth, surname_ipa) AS (
-    SELECT *
-    FROM (VALUES
-        (1, 'William Shatner',  '1931-03-22', NULL),
+/* DuckDB */
+with mock_data(id, name, date_of_birth, surname_ipa) as (
+    values
+        (1, 'William Shatner',  '1931-03-22', null),
         (2, 'Leonard Nimoy',    '1931-03-26', '/ˈniːmɔɪ/'),
         (3, 'Nichelle Nichols', '1932-12-28', '/nɪˈʃɛl/')
-    )
 )
 
-SELECT
-    CAST(id AS INTEGER) AS id,
-    CAST(name AS TEXT) AS name,
-    CAST(date_of_birth AS TEXT) AS date_of_birth,
-    CAST(surname_ipa AS TEXT) AS surname_ipa
-FROM mock_data
+select
+    cast(id as integer) as id,
+    cast(name as text) as name,
+    cast(date_of_birth as text) as date_of_birth,
+    cast(surname_ipa as text) as surname_ipa
+from mock_data
 ;
